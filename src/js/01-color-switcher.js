3df1fa5d9bf35a7b4836dfe95ca863133cec9946
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2023-01-03 17:47:47 +0200

    complete task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+let timerId = null;
+
+function onStart() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    changeColor();
+}
+function changeColor() {
+        timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        body.setAttribute("style", `background-color: ${color}`);
+    }, 1000)
+}
+function onStop() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(timerId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+// const refs = {
+//     startBtn: document.querySelector('button[data-start]'),
+//     input: document.querySelector('input'),
+
+//     days: document.querySelector('span[data-days]'),
+//     hours: document.querySelector('span[data-hours]'),
+//     minutes: document.querySelector('span[data-minutes]'),
+//     seconds: document.querySelector('span[data-seconds]'),
+// }
+const startBtn = document.querySelector('button[data-start]');
+// startBtn.addEventListener('click', () => {
+//     console.log(1);
+// })
+console.log(startBtn);
