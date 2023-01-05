09d28ae62f5619346ca22eded6aa612db3e4c711
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2023-01-05 16:29:05 +0200

    03-promises (complete)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const { delay, step, amount } = form.elements;
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  createPromisesByAmount();
+}
+
+function createPromisesByAmount() {
+  const target = Number(amount.value);
+  let currentDelay = Number(delay.value);
+  const postDelay = Number(step.value);
+
+
+  for (let i = 1; i <= target; i += 1) {
+    currentDelay += postDelay;
+
+    createPromise(i,currentDelay)
+      .then(({ position, delay }) => {
+      Notiflix.Notify.warning(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      if (shouldResolve) {
+      resolve({ position, delay })
+      }
+      reject({ position, delay })
+    }, delay)
+
+})
+
 }
